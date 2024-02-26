2d737219773f7a69d9c1bddf5f12d90128f829e5
unknown <02230111.cst@rub.edu.bt>
2024-02-26 15:12:33 +0600

    logical condition

--- logical.py
+++ logical.py
@@ -1,6 +1,17 @@
 x = 5
-print(x > 3 and x < 10)  # True because both the conditions are True
+print(x > 3 and x < 10) 
 
 y = 12
-print(y > 10 and y % 5 == 0)  # False because the second condition is false
- 
+print(y > 10 and y % 5 == 0) 
+
+x = 5
+print(x < 10 or x >10)
+
+y = 12
+print(y < 10 or y % 2 == 0)
+
+x = 5
+print(not(x > 3 and x < 10))
+
+y = 12
+print(not(y >10 and y % 5))
